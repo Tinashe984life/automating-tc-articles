d88ba39a9b246973039bd8c7158229888f62680a
Tinashe Madanire <madtinashe@gmail.com>
2022-10-23 21:07:42 +0200

    added grabArticle function
    Added a function which crawls to each of the collected Articles and grabs the article content and prints it to the console.

--- crawlr.py
+++ crawlr.py
@@ -4,7 +4,7 @@ import requests
 
 links = []
 
-class Article:
+class Article: # Article Class 
     def __init__(self, name, url):
         self.name = name
         self.url = url
@@ -12,19 +12,28 @@ class Article:
     def __repr__(self):
         return (f'Article Name: {self.name} \n Article URL: {self.url}')
 
-def getLinks():
+def getLinks(): # Grabs the latest stories , Headlines + URLs
     html_text = requests.get('https://techcrunch.com/').text
     bs = BeautifulSoup(html_text, 'lxml')
     container = bs.find('div', class_='river river--homepage')
-    #print(Fore.RED, container)
     articles = container.find_all('a', class_='post-block__title__link')
     for article in articles:    
         articleName = article.text.strip()
         articleUrl = article.attrs['href']
         newArticle = Article(articleName, articleUrl)
         links.append(newArticle)
-        #print(Fore.GREEN, f'Article: {articleName} \n {Fore.YELLOW}URL: {articleUrl}')
         
     print(Fore.GREEN, 'Articles Successfully collected')
 
+def grabArticle(): # Goes to each article url and grabs the article text 
+    for link in links:
+        print(Fore.RED, link.name)
+        html_text = requests.get(link.url).text
+        soup = BeautifulSoup(html_text, 'lxml')
+        article = soup.find('div', class_='article-content')
+        paragraphs = article.find_all('p')
+        for p in paragraphs:
+            print(Fore.YELLOW, p.text)
+
 getLinks()
+grabArticle()
